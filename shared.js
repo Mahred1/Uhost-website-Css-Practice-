e8c8592c1fe45231373daa770f8d94d14b57b8f4
mredwan <mredwan.kemal@gmail.com>
2024-08-23 07:01:06 -0900

    added a mobile nav

--- shared.js
+++ shared.js
@@ -1,19 +1,17 @@
 var backdrop = document.querySelector('.back-drop');
 var modal = document.querySelector('.modal');
-var modalNo = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 for (let i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click',function(){
+    selectPlanButtons[i].addEventListener('click',function() {
         backdrop.style.display='block';
         modal.style.display='block';
     })
-backdrop.addEventListener('click',function(){
-    backdrop.style.display='none';
-    modal.style.display='none';
-})
-modalNo.addEventListener('click',function(){
-    backdrop.style.display='none';
-    modal.style.display='none';
-})
-    
 }
+backdrop.addEventListener('click',closeModal);
+modal.addEventListener('click',closeModal);
+
+function closeModal(){
+    backdrop.style.display='none';
+    modal.style.display='none';
+}
+    
