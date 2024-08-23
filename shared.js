adba00ec792d572f33fe79ac322499f90973a8b7
mredwan <mredwan.kemal@gmail.com>
2024-08-23 09:39:21 -0900

    added side navigation bar

--- shared.js
+++ shared.js
@@ -1,17 +1,26 @@
 var backdrop = document.querySelector('.back-drop');
 var modal = document.querySelector('.modal');
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav =document.querySelector('.mobile-nav');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click',function() {
-        backdrop.style.display='block';
-        modal.style.display='block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     })
 }
-backdrop.addEventListener('click',closeModal);
+toggleButton.addEventListener('click',function(){
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+})
+backdrop.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+    closeModal();
+});
 modal.addEventListener('click',closeModal);
 
 function closeModal(){
-    backdrop.style.display='none';
-    modal.style.display='none';
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
 }
     
