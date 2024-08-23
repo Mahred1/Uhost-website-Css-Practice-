e348a620f1b5560438063126c36824db9eca44a4
mredwan <mredwan.kemal@gmail.com>
2024-08-23 06:22:30 -0900

    added event listner to the modal

--- shared.js
+++ shared.js
@@ -1,3 +1,19 @@
 var backdrop = document.querySelector('.back-drop');
-
-backdrop.style.display = 'block';
+var modal = document.querySelector('.modal');
+var modalNo = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+for (let i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click',function(){
+        backdrop.style.display='block';
+        modal.style.display='block';
+    })
+backdrop.addEventListener('click',function(){
+    backdrop.style.display='none';
+    modal.style.display='none';
+})
+modalNo.addEventListener('click',function(){
+    backdrop.style.display='none';
+    modal.style.display='none';
+})
+    
+}
